2bb231434b82ed368db681a81448773d69ff42d0
Pine Mizune <pinemz@gmail.com>
2026-06-10 20:53:18 +0900

    style: fix indent

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 interface CloneIntoOptions {
-    cloneFunctions: boolean;
-    wrapReflectors: boolean;
+  cloneFunctions: boolean;
+  wrapReflectors: boolean;
 }
 
 type CloneInto = <T>(obj: T, targetScope: any, options?: CloneIntoOptions) => T;
 
 function getCloneInto() : CloneInto {
+  // @ts-ignore
+  if (typeof globalThis.cloneInto === 'function') {
     // @ts-ignore
-    if (typeof globalThis.cloneInto === 'function') {
-        // @ts-ignore
-        return globalThis.cloneInto
-    }
+    return globalThis.cloneInto
+  }
 
-    return <T>(obj: T, targetScope: any, options?: CloneIntoOptions) => {
-        return obj
-    }
+  return <T>(obj: T, targetScope: any, options?: CloneIntoOptions) => {
+    return obj
+  }
 }
 
 export const cloneInto = getCloneInto()
